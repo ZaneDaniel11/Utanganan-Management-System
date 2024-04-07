ae7a1a4528a07df717520db0755856b64a232683
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-04-07 23:42:48 +0800

    new
    This is new

--- Entities/ClientsInfoTb.cs
+++ Entities/ClientsInfoTb.cs
@@ -17,7 +17,7 @@ public partial class ClientsInfoTb
 
     public int ZipCode { get; set; }
 
-    public DateTime Birthday { get; set; }
+    public DateTime Birthday { get; set; } 
 
     public int Age { get; set; }
 
--- obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d584f166e2063e589637f69452d08c364302edf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b18cc0e8135a1e2f9e31cd0b1986bd8f7e211a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
