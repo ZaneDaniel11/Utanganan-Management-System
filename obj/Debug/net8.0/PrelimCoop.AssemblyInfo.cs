210837bcbdc6db176e8fba8b928e7f4fb4edf909
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-04 22:05:14 +0800

    shesh

--- obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee63e3e2571e399cc4df59aa51c597399901f22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7dc9639f48cbf5500191046ca02e70b47a3cfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
