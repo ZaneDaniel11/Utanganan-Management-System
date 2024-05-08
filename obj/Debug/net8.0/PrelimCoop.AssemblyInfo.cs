30496d5ade377c30730eb640cf9e7dc7479fda79
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-08 10:30:41 +0800

    Form Design

--- obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97bef2d5648c3095c9bd8974f442ab6ea66d31f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6684b37e08913cb0a21fa6458432ec3ba545ab8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
