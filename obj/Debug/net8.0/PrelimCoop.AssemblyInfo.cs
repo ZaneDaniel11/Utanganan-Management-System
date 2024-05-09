ce042fd17bb9920ba797322a0f93dd7bc86df5fd
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-09 22:12:36 +0800

    added Table

--- obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30496d5ade377c30730eb640cf9e7dc7479fda79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2ede828d8fc25d6ec55d6d7b55814889f93559")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/table.js
@@ -0,0 +1,2 @@
+	
+new DataTable('#load-table');
