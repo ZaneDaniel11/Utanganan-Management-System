1e6b35315b2e40b1c0c4bfc96b7382cce3650a6c
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-12 20:34:00 +0800

    added shit

--- obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df1577025881bd6df6082a35bb959f1ff4dea48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340186a6631d4f652205f56415e2a0cabc3a041c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
