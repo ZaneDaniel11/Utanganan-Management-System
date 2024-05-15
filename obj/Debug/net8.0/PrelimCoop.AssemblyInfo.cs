cb82c59c69641af50f5c5022c2a98ab87a17cc08
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-16 00:46:33 +0800

    Fixed Bug

--- obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrelimCoop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130eb1db46e6676e8a7e200654b46618bccad4c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c54afaecd5b5769710f4668cc3124874991a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrelimCoop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
