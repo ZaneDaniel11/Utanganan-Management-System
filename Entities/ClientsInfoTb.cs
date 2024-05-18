314abab016b9079701f2650c7363b89336e5d1ec
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-18 23:06:47 +0800

    Update ClientsInfoTb.cs

--- Entities/ClientsInfoTb.cs
+++ Entities/ClientsInfoTb.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrelimCoop.Entities;
+
+public partial class ClientsInfoTb
+{
+    public int Id { get; set; }
+
+    public string UserType { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public int ZipCode { get; set; }
+
+    public string Birthday { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public string NameOfFather { get; set; } = null!;
+
+    public string NameOfMother { get; set; } = null!;
+
+    public string CivilStatus { get; set; } = null!;
+
+    public string Religion { get; set; } = null!;
+
+    public string Occupation { get; set; } = null!;
+}
